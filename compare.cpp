0ba67d821acf7708c2aadba45825d08765a3ce1b
Chiluveru Girish <144055051+chiluverugirish@users.noreply.github.com>
2025-01-20 15:22:25 +0530

    Create compare.cpp

---
+++ compare.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 3;
+    if (a > b) {
+        cout << "A is greater." << endl;
+    } else {
+        cout << "B is greater." << endl;
+    }
+    return 0;
+}
